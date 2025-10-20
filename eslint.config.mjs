7c1305d5910acf43db23dc42efa7ce108b733b74
gabri <josegabrielsantana19@gmail.com>
2025-10-19 23:44:21 -0300

    resolvendo bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
@@ -19,6 +20,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/no-unescaped-entities": "off"
+    }
   },
 ];
 
